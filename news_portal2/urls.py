399d6796d06bc7499a0910a54b9623f48f87dcc5
Dmitry <dmitriymllr6@gmail.com>
2026-06-04 23:21:48 +0500

    Practical assignment 2 - Django news portal with auth

--- news_portal2/urls.py
+++ news_portal2/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('news_app.urls')),
+    path('', include('news_app2.urls')),
     path('admin/', admin.site.urls),
 ]
